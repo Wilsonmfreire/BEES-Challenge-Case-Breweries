f6a7bbe633e5a522efa2818c856aa11e04c6e353
Wilson Morais Freire <62375886+Wilsonmfreire@users.noreply.github.com>
2024-12-18 15:27:57 -0300

    Update silver_dag.py
    fixe

--- dados_cervejarias/pipeline/silver_dag.py
+++ dados_cervejarias/pipeline/silver_dag.py
@@ -12,17 +12,17 @@ DAG_FOLDER = os.path.dirname(os.path.abspath(__file__))
 SCRIPTS_DIR = os.path.join(DAG_FOLDER, "..", "dados_cervejarias", "data_injestion", "script")
 sys.path.append(SCRIPTS_DIR)
 
-import load_gold
+import transform_silver
 
 with DAG(
-    dag_id="gold_layer",
+    dag_id="silver_layer",
     schedule=None,  # Set a schedule if desired
     start_date=pendulum.datetime(2023, 10, 26, tz="UTC"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
-    tags=["gold", "loading"],
+    tags=["silver", "transformation"],
 ) as dag:
-    load_gold_task = PythonOperator(
-        task_id="load_gold_data",
-        python_callable=load_gold.main,
+    transform_silver_task = PythonOperator(
+        task_id="transform_silver_data",
+        python_callable=transform_silver.main,
     )

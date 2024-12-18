b4e48e56bca74b3b1dbf5e224aba18acdda6746c
Wilson Morais Freire <62375886+Wilsonmfreire@users.noreply.github.com>
2024-12-18 15:20:31 -0300

    Update bronze_dag.py
    fix

--- dados_cervejarias/pipeline/bronze_dag.py
+++ dados_cervejarias/pipeline/bronze_dag.py
@@ -1,11 +1,18 @@
 from __future__ import annotations
+
 import datetime
 import pendulum
+import os
+import sys
 
 from airflow.models.dag import DAG
-from airflow.providers.papermill.operators.papermill import PapermillOperator
+from airflow.operators.python import PythonOperator
 
 DAG_FOLDER = os.path.dirname(os.path.abspath(__file__))
+SCRIPTS_DIR = os.path.join(DAG_FOLDER, "..", "dados_cervejarias", "data_injestion", "script")
+sys.path.append(SCRIPTS_DIR)
+
+import extract_bronze
 
 with DAG(
     dag_id="bronze_layer",
@@ -15,11 +22,7 @@ with DAG(
     dagrun_timeout=datetime.timedelta(minutes=60),
     tags=["bronze", "ingestion"],
 ) as dag:
-    extract_from_api_to_json = PapermillOperator(
-        task_id="extract_api_to_json",
-        input_nb=os.path.join(DAG_FOLDER, "..", "data_injection", "script", "bronze_layer.py"),
-        output_nb="/tmp/bronze_output.py",  # Opcional
-        parameters={
-            "config_path": os.path.join(DAG_FOLDER, "..", "data_injection", "config.json"),
-        },
+    extract_bronze_task = PythonOperator(
+        task_id="extract_bronze_data",
+        python_callable=extract_bronze.main,
     )

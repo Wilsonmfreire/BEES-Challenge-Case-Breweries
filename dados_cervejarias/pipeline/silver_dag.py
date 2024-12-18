613d843d3af05db607289f743ec41ba168ce7c74
Wilson Morais Freire <62375886+Wilsonmfreire@users.noreply.github.com>
2024-12-18 15:25:40 -0300

    Update silver_dag.py
    fix

--- dados_cervejarias/pipeline/silver_dag.py
+++ dados_cervejarias/pipeline/silver_dag.py
@@ -1,27 +1,28 @@
 from __future__ import annotations
 
 import datetime
-
 import pendulum
+import os
+import sys
 
 from airflow.models.dag import DAG
-from airflow.providers.papermill.operators.papermill import PapermillOperator
+from airflow.operators.python import PythonOperator
 
 DAG_FOLDER = os.path.dirname(os.path.abspath(__file__))
+SCRIPTS_DIR = os.path.join(DAG_FOLDER, "..", "dados_cervejarias", "data_injestion", "script")
+sys.path.append(SCRIPTS_DIR)
+
+import load_gold
 
 with DAG(
-    dag_id="silver_layer",
-    schedule=None, 
+    dag_id="gold_layer",
+    schedule=None,  # Set a schedule if desired
     start_date=pendulum.datetime(2023, 10, 26, tz="UTC"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
-    tags=["silver", "transformation"],
+    tags=["gold", "loading"],
 ) as dag:
-    extract_from_api_to_json = PapermillOperator(
-        task_id="transform_data",
-        input_nb=os.path.join(DAG_FOLDER, "..", "data_injection", "notebook", "silver_layer.ipynb")
-        output_nb="/tmp/silver_output.ipynb",
-        parameters={
-            "config_path": os.path.join(DAG_FOLDER, "..", "data_injection", "config.json"),
-        },
+    load_gold_task = PythonOperator(
+        task_id="load_gold_data",
+        python_callable=load_gold.main,
     )
